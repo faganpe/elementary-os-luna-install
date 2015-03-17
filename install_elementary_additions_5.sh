c900070b21afbceed74b163cd165f675d3a98330
faganpe <paulfagan@hotmail.com>
2015-03-17 22:44:05 +0000

    Update install_elementary_additions_5.sh

--- install_elementary_additions_5.sh
+++ install_elementary_additions_5.sh
@@ -1,1 +1,2 @@
 sudo sh -c "echo 'deb http://download.virtualbox.org/virtualbox/debian 'utopic' contrib non-free' > /etc/apt/sources.list.d/virtualbox.list" && wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add - && sudo apt-get update && sudo apt-get install virtualbox-4.3 dkms
+sudo apt-get -y install maven
