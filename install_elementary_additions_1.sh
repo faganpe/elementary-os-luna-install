746bf12e7efa8e9738bfa54db95932466bea57d0
faganpe <paulfagan@hotmail.com>
2015-03-17 20:41:30 +0000

    Update install_elementary_additions_1.sh

--- install_elementary_additions_1.sh
+++ install_elementary_additions_1.sh
@@ -1,23 +1,23 @@
 sudo software-properties-gtk
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get dist-upgrade
-sudo apt-get install aptitude synaptic gdebi-core
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y dist-upgrade
+sudo apt-get -y install aptitude synaptic gdebi-core
 sudo apt-add-repository ppa:versable/elementary-update -y
-sudo apt-get update
-sudo apt-get install elementary-tweaks
-sudo apt-get install elementary-wallpaper-collection
-sudo apt-get install wingpanel-slim super-wingpanel
-sudo apt-get install elementary-.*-theme elementary-.*-icons
-sudo apt-get install indicator-synapse
-sudo apt-get install firefox
-sudo apt-get install chromium-browser
+sudo apt-get -y update
+sudo apt-get -y install elementary-tweaks
+sudo apt-get -y install elementary-wallpaper-collection
+sudo apt-get -y install wingpanel-slim super-wingpanel
+sudo apt-get -y install elementary-.*-theme elementary-.*-icons
+sudo apt-get -y install indicator-synapse
+sudo apt-get -y install firefox
+sudo apt-get -y install chromium-browser
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo gdebi google-chrome-stable_current_amd64.deb
 #sudo apt-get install adobe-flashplugin
-sudo apt-get install vlc
-sudo apt-get install ubuntu-restricted-extras libavcodec-extra-53
-sudo apt-get install libdvdread4
+sudo apt-get -y install vlc
+sudo apt-get -y install ubuntu-restricted-extras libavcodec-extra-53
+sudo apt-get -y install libdvdread4
 sudo /usr/share/doc/libdvdread4/install-css.sh
-sudo apt-get install unace rar unrar p7zip-rar p7zip sharutils uudeview mpack lha arj cabextract lzip lunzip
-sudo apt-get install skype
+sudo apt-get -y install unace rar unrar p7zip-rar p7zip sharutils uudeview mpack lha arj cabextract lzip lunzip
+sudo apt-get -y install skype
