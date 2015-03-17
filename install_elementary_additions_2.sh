a44bc7bd6b16c9f8161992c4aab5acdd5021d1ab
faganpe <paulfagan@hotmail.com>
2015-03-17 21:47:38 +0000

    Update install_elementary_additions_2.sh

--- install_elementary_additions_2.sh
+++ install_elementary_additions_2.sh
@@ -1,3 +1,13 @@
-sudo add-apt-repository ppa:webupd8team/java
-sudo apt-get update
-sudo apt-get -y install oracle-java7-installer
+sudo gdebi dropbox_2015.02.12_amd64.deb
+sudo apt-get -y install transmission
+sudo apt-get -y install qbittorrent
+sudo apt-get -y install deluge
+sudo apt-get -y install uget
+sudo apt-get -y install libreoffice
+sudo apt-get -y install gimp
+sudo apt-get -y install pidgin
+sudo apt-get -y install glipper
+sudo apt-get -y install guake
+sudo apt-get -y install icedtea-plugin
+sudo sed -i 's/NoDisplay=true/NoDisplay=false/g' /etc/xdg/autostart/*.desktop
+gksudo pantheon-files
