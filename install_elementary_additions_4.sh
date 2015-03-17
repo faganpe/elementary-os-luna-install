a4e59f7567d4878c25d375a61520db47e4ffe015
faganpe <paulfagan@hotmail.com>
2015-03-17 21:55:36 +0000

    Create install_elementary_additions_4.sh

---
+++ install_elementary_additions_4.sh
@@ -0,0 +1,4 @@
+sudo apt-get -y install curl
+curl -sSL https://get.docker.com/ubuntu/ | sudo sh
+echo "Now run this command to try docker..."
+echo "sudo docker run -i -t ubuntu /bin/bash"install_elementary_additions_4.sh
